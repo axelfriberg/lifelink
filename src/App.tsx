becfc68f31521396970845fe223313120d8e4551
Axel Friberg <hello@axelfriberg.com>
2023-12-03 16:04:06 +0100

    Install and run prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
-import './App.css'
-import {LifeCounter} from "./components/LifeCounter.tsx";
+import "./App.css";
+import { LifeCounter } from "./components/LifeCounter.tsx";
 
 const STARTING_LIFE_TOTAL = 20;
 
 function App() {
-
   return (
     <>
-      <LifeCounter className="flip" startingLifeTotal={STARTING_LIFE_TOTAL} playerName="Player 1"/>
-      <hr/>
-        <LifeCounter startingLifeTotal={STARTING_LIFE_TOTAL} playerName="Player 2"/>
+      <LifeCounter
+        className="flip"
+        startingLifeTotal={STARTING_LIFE_TOTAL}
+        playerName="Player 1"
+      />
+      <hr />
+      <LifeCounter
+        startingLifeTotal={STARTING_LIFE_TOTAL}
+        playerName="Player 2"
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
