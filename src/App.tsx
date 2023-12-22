ee2d7f7d8afb29fbb8e9fe814d93f6305522d896
Axel Friberg <hello@axelfriberg.com>
2023-12-22 17:28:07 +0100

    Add start stop to timer

--- src/App.tsx
+++ src/App.tsx
@@ -5,23 +5,44 @@ import useRoundTimer from "./useRoundTimer.ts";
 export const STARTING_LIFE_TOTAL = 20;
 
 function App() {
-  const { minutes, seconds } = useRoundTimer();
+  const { minutes, seconds, start, stop, isRunning } = useRoundTimer();
+  const toggleTimer = () => {
+    if (isRunning) {
+      stop();
+    } else {
+      start();
+    }
+  };
   return (
     <div>
       <div className="flip">
-        <p className="round-timer">
-          {minutes}:{seconds}
-        </p>
+        <div className="round-timer">
+          <p className="clock">{`${String(minutes).padStart(2, "0")}:${String(
+            seconds,
+          ).padStart(2, "0")}`}</p>
+          <button type="button" onClick={toggleTimer}>
+            {isRunning ? "Stop timer" : "Start timer"}
+          </button>
+        </div>
         <LifeCounter
           startingLifeTotal={STARTING_LIFE_TOTAL}
           playerName="Player 2"
         />
       </div>
       <hr className="divider" />
+
       <div>
-        <p className="round-timer">
-          {minutes}:{seconds}
-        </p>
+        <div className="round-timer">
+          <p className="clock">
+            {`${String(minutes).padStart(2, "0")}:${String(seconds).padStart(
+              2,
+              "0",
+            )}`}
+          </p>
+          <button type="button" onClick={toggleTimer}>
+            {isRunning ? "Stop timer" : "Start timer"}
+          </button>
+        </div>
         <LifeCounter
           startingLifeTotal={STARTING_LIFE_TOTAL}
           playerName="Player 1"
--- src/useRoundTimer.ts
+++ src/useRoundTimer.ts
@@ -2,25 +2,35 @@ import { useState, useEffect } from "react";
 
 const useRoundTimer = () => {
   const [time, setTime] = useState(50 * 60 * 1000); // 50 minutes in ms
+  const [isRunning, setIsRunning] = useState(false);
 
   useEffect(() => {
-    const timer = setInterval(() => {
-      setTime((prevTime) => prevTime - 1000);
-    }, 1000);
+    let timer: number;
+
+    if (isRunning) {
+      timer = setInterval(() => {
+        setTime((prevTime) => prevTime - 1000);
+      }, 1000);
+    }
 
     return () => {
       clearInterval(timer);
     };
-  }, []);
+  }, [isRunning]);
 
   const date = new Date(time);
 
   const minutes = date.getUTCMinutes();
   const seconds = date.getSeconds();
+  const start = () => setIsRunning(true);
+  const stop = () => setIsRunning(false);
 
   return {
-    minutes: minutes,
-    seconds: seconds,
+    minutes,
+    seconds,
+    start,
+    stop,
+    isRunning,
   };
 };
 
