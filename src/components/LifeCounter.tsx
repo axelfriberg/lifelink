f7a1c5c811a58d853aa6a6074abd0abc8c0a75aa
Axel Friberg <hello@axelfriberg.com>
2024-01-24 21:36:27 +0100

    Increase size of life buttons

--- src/components/LifeCounter.tsx
+++ src/components/LifeCounter.tsx
@@ -21,22 +21,22 @@ export function LifeCounter({
     <div className={className}>
       <div className="flex flex-col w-full gap-4 items-center">
         <button
-          className="text-4xl w-full flex justify-center px-16 py-4 bg-slate-600 text-white rounded-lg active:bg-slate-500"
+          className="text-4xl w-full flex justify-center py-6 bg-slate-600 text-white rounded-lg active:bg-slate-500"
           type="button"
           onClick={onLifeIncrease}
         >
-          <FaPlus title="Increase life"/>
+          <FaPlus title="Increase life" />
         </button>
         <div className="grid grid-cols-3 text-5xl font-bold min-w-80 justify-items-center">
           <div className="col-start-2">{life}</div>
           <CurrentLifeChange currentLifeChange={currentLifeChange} />
         </div>
         <button
-          className="text-4xl w-full flex justify-center px-16 py-4 bg-slate-600 text-white rounded-lg active:bg-slate-500"
+          className="text-4xl w-full flex justify-center py-6 bg-slate-600 text-white rounded-lg active:bg-slate-500"
           type="button"
           onClick={onLifeDecrease}
         >
-          <FaMinus title="Decrease life"/>
+          <FaMinus title="Decrease life" />
         </button>
       </div>
     </div>
